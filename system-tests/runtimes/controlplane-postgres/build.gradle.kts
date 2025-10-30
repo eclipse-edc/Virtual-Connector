490280a28ee2312fad3924052a34d804fa397b0a
Enrico Risa <enrico.risa@gmail.com>
2025-10-30 17:53:21 +0100

    chore: plug SQL participant context store

--- system-tests/runtimes/controlplane-postgres/build.gradle.kts
+++ system-tests/runtimes/controlplane-postgres/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     runtimeOnly(project(":system-tests:runtimes:controlplane-base"))
     runtimeOnly(libs.edc.vault.hashicorp)
     runtimeOnly(libs.edc.bom.controlplane.sql)
+    runtimeOnly(libs.edc.participantcontext.store.sql)
 }
 
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
