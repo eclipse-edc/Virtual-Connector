89c6ad25b2d1ccf089ec2f5f49d029a775b387a3
Enrico Risa <enrico.risa@gmail.com>
2025-11-14 09:45:18 +0100

    feat: contract negotiation APIs (#51)

---
+++ extensions/control-plane/api/management-api/contract-negotiation-api/src/test/java/org/eclipse/edc/virtual/connector/controlplane/api/management/contractnegotiation/v4/ContractNegotiationApiV4ControllerTest.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2025 Metaform Systems, Inc.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Metaform Systems, Inc. - initial API and implementation
+ *
+ */
+
+package org.eclipse.edc.virtual.connector.controlplane.api.management.contractnegotiation.v4;
+
+import org.eclipse.edc.junit.annotations.ApiTest;
+import org.eclipse.edc.virtual.connector.controlplane.api.management.contractnegotiation.ContractNegotiationApiControllerTestBase;
+
+@ApiTest
+class ContractNegotiationApiV4ControllerTest extends ContractNegotiationApiControllerTestBase {
+
+    @Override
+    protected Object controller() {
+        return new ContractNegotiationApiV4Controller(service, participantContextService, authorizationService, transformerRegistry, monitor);
+    }
+
+    @Override
+    protected String versionPath() {
+        return "v4alpha";
+    }
+}
