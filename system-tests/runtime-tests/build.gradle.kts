a1d3f4d70dc3bfd02c6499bf630e04410ccca6c6
Enrico Risa <enrico.risa@gmail.com>
2026-04-24 12:27:02 +0200

    chore: remove transfer tests

--- system-tests/runtime-tests/build.gradle.kts
+++ system-tests/runtime-tests/build.gradle.kts
@@ -18,40 +18,12 @@ plugins {
 
 dependencies {
     testImplementation(libs.awaitility)
-    testImplementation(libs.edc.spi.cel)
-    testImplementation(libs.edc.spi.jsonld)
-    testImplementation(libs.edc.spi.transaction.datasource)
-    testImplementation(libs.edc.spi.controlplane)
-    testImplementation(libs.edc.spi.dataplane.selector)
-    testImplementation(libs.edc.spi.participantcontext.config)
-    testImplementation(libs.edc.lib.jsonld)
-    testImplementation(libs.edc.lib.sql)
     testImplementation(libs.edc.junit)
     testImplementation(libs.restAssured)
     testImplementation(testFixtures(libs.edc.fixtures.sql))
     testImplementation(testFixtures(libs.edc.lib.nats))
-    testImplementation(project(":system-tests:system-test-fixtures"))
-    testImplementation(testFixtures(libs.edc.ih.test.fixtures)) {
-        exclude("com.networknt", "json-schema-validator")
-    }
     testImplementation(libs.testcontainers.junit)
-    testImplementation(libs.testcontainers.vault)
     testImplementation(libs.testcontainers.postgres)
-    testImplementation(libs.wiremock) {
-        exclude("com.networknt", "json-schema-validator")
-    }
-    testImplementation(testFixtures(libs.edc.lib.oauth2.authn))
-    testImplementation(testFixtures(libs.edc.fixtures.mgmtapi))
-    testImplementation(libs.nimbus.jwt)
-    testImplementation(libs.bouncyCastle.bcpkixJdk18on)
-
-    testCompileOnly(project(":system-tests:runtimes:e2e:e2e-controlplane-memory"))
-    testCompileOnly(project(":system-tests:runtimes:e2e:e2e-controlplane-postgres"))
-    testCompileOnly(project(":system-tests:runtimes:e2e:e2e-controlplane-postgres-nats"))
-    testCompileOnly(project(":system-tests:runtimes:e2e:e2e-dcp-controlplane-postgres"))
-    testCompileOnly(project(":system-tests:runtimes:issuer"))
-    testCompileOnly(project(":system-tests:runtimes:identity-hub"))
-
 }
 
 edcBuild {
